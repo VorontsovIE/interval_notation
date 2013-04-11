ad287e2646a6ff53b42f0c1c6091bdab22e82453
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-04-12 01:27:47 +0400

    more tests for Region. Some methods made private (and some are marked as candidates to be removed)

--- lib/region.rb
+++ lib/region.rb
@@ -1,27 +1,26 @@
-$:.unshift File.dirname(File.expand_path(__FILE__))
-
 # [pos_start; pos_end) region on specified chromosome and strand
-# It's required that pos_start <= pos_end
+# It's required that pos_start < pos_end
 class Region
-  attr_reader :chromosome, :strand, :pos_start, :pos_end, :region
-  
+  attr_reader :chromosome, :strand, :pos_start, :pos_end
+
   def ==(other_region)
     chromosome == other_region.chromosome && strand == other_region.strand && pos_start == other_region.pos_start && pos_end == other_region.pos_end
   end
   def eql?(other_region)
     self == other_region
   end
+
   def hash
     annotation.hash
   end
-  
+
   # region represented a semi-interval: [pos_start; pos_end) Positions are 0-based
   def initialize(chromosome, strand, pos_start, pos_end)
     @chromosome, @strand, @pos_start, @pos_end = chromosome, strand, pos_start, pos_end
-    @region = pos_start...pos_end
+
     raise "Negative length for region #{annotation}"  if length <= 0
   end
-  
+
   # Region.new_by_annotation('chr1:564462..564463,+')
   def self.new_by_annotation(name)
     chromosome, name = name.split(':')
@@ -35,18 +34,7 @@ class Region
 
   alias_method :to_s, :annotation
   alias_method :inspect, :annotation
-  
-  def same_strand?(other_region)
-    other_region.chromosome == chromosome && other_region.strand == strand
-  end
-  # whether pos is inside of region provided that strand and chromosome are the same
-  def include_position?(pos)
-    @region.include?(pos)
-  end
-  # whether region is inside of region
-  def include?(other_region)
-    same_strand?(other_region) && include_position?(other_region.pos_start) && (include_position?(other_region.pos_end) || pos_end == other_region.pos_end)
-  end
+
   def intersect?(other_region)
     #same_strand?(other_region) && (include?(other_region) || other_region.include?(self) || include_position?(other_region.pos_start) || (include_position?(other_region.pos_end) && other_region.pos_end != pos_start))
     same_strand?(other_region) && !(pos_start >= other_region.pos_end || other_region.pos_start >= pos_end)
@@ -56,17 +44,17 @@ class Region
     return nil unless same_strand?(other_region)
     return nil unless intersect?(other_region)
     # self is [], other_region is ()
-    
+
     if pos_start < other_region.pos_start && other_region.include_position?(pos_end) && other_region.pos_start != pos_end
       # [ ( ] )
       Region.new(chromosome, strand, other_region.pos_start, pos_end)
     elsif pos_end > other_region.pos_end && other_region.include_position?(pos_start)
       # ( [ ) ]
       Region.new(chromosome, strand, pos_start, other_region.pos_end)
-    elsif other_region.include?(self)
+    elsif other_region.contain?(self)
       # ( [ ] )
       self
-    elsif include?(other_region)
+    elsif contain?(other_region)
       # [ ( ) ]
       other_region
     else
@@ -77,7 +65,7 @@ class Region
   def length
     pos_end - pos_start
   end
-  
+
   # genome_dir is a folder with files of different chromosomes
   # here we don't take strand into account
   def load_sequence(genome_dir)
@@ -86,6 +74,30 @@ class Region
       f.seek(pos_start)
       f.read(length)
     end
+  end
+
+
+
+  # whether other_region is inside of region
+  def contain?(other_region)
+    same_strand?(other_region) && include_position?(other_region.pos_start) && (include_position?(other_region.pos_end) || pos_end == other_region.pos_end)
+  end
+
+  def same_strand?(other_region)
+    other_region.chromosome == chromosome && other_region.strand == strand
+  end
+  private :same_strand?
+
+  # whether pos is inside of region provided that strand and chromosome are the same
+  def include_position?(pos)
+    region.include?(pos)
+  end
+  #private :include_position?
+
+
+  # method to be removed
+  def region
+    @region ||= pos_start...pos_end
   end
 
 end
--- spec/region_spec.rb
+++ spec/region_spec.rb
@@ -21,7 +21,7 @@ describe Region do
   Given(:region) { Region.new('chr1', '+', 100, 110) }
   Given(:region_inside) { Region.new('chr1', '+', 103, 107) }
   Given(:region_inside_extend_to_end) { Region.new('chr1', '+', 103, 110) }
-  Given(:region_inside_extend_to_start) { Region.new('chr1', '+', 100, 103) }
+  Given(:region_inside_extend_from_start) { Region.new('chr1', '+', 100, 103) }
 
   Given(:region_on_another_strand) { Region.new('chr1', '-', 103, 107) }
   Given(:region_on_another_chromosome) { Region.new('chr2', '+', 103, 107) }
@@ -35,14 +35,40 @@ describe Region do
   Given(:region_outside_right) { Region.new('chr1', '+', 113, 117) }
   Given(:region_outside_extend_from_end) { Region.new('chr1', '+', 110, 113) }
   Given(:region_outside_extend_to_start) { Region.new('chr1', '+', 97, 100) }
-  
-  #Given(:empty_region) { Region.new('chr1', '+', 103, 103) }
+
+  Given(:all_region_types) {
+    [ region, region_inside, region_inside_extend_to_end, region_inside_extend_from_start,
+    region_on_another_strand, region_on_another_chromosome,
+    region_from_inside_to_outside_after_end, region_from_outside_before_start_to_inside,
+    region_containing,
+    region_outside_left, region_outside_right,
+    region_outside_extend_from_end, region_outside_extend_to_start]
+  }
+
+  describe '#intersect?' do
+    Then{
+      all_region_types.each{|second_region|
+        region.intersect?(second_region).should == second_region.intersect?(region)
+      }
+    }
+    Then{
+      all_region_types.each{|second_region|
+        region.intersect?(second_region).should be_true  if region.intersection(second_region)
+      }
+    }
+    Then{
+      all_region_types.each{|second_region|
+        region.intersect?(second_region).should be_false  unless region.intersection(second_region)
+      }
+    }
+  end
 
   describe '#intersection' do
     Then{ region.intersection(region).should == region }
+
     Then{ region.intersection(region_inside).should == region_inside }
     Then{ region.intersection(region_inside_extend_to_end).should == region_inside_extend_to_end }
-    Then{ region.intersection(region_inside_extend_to_start).should == region_inside_extend_to_start }
+    Then{ region.intersection(region_inside_extend_from_start).should == region_inside_extend_from_start }
 
     Then{ region.intersection(region_on_another_strand).should == nil }
     Then{ region.intersection(region_on_another_chromosome).should == nil }
@@ -56,5 +82,82 @@ describe Region do
     Then{ region.intersection(region_outside_right).should == nil }
     Then{ region.intersection(region_outside_extend_from_end).should == nil }
     Then{ region.intersection(region_outside_extend_to_start).should == nil }
+
+    Then{
+      all_region_types.each{|second_region|
+        region.intersection(second_region).should == second_region.intersection(region)
+      }
+    }
+  end
+
+  describe '#contain?' do
+    Then{ region.contain?(region).should be_true }
+
+    Then{ region.contain?(region_inside).should be_true }
+    Then{ region.contain?(region_inside_extend_to_end).should be_true }
+    Then{ region.contain?(region_inside_extend_from_start).should be_true }
+
+    Then{ region.contain?(region_on_another_strand).should  be_false }
+    Then{ region.contain?(region_on_another_chromosome).should be_false }
+
+    Then{ region.contain?(region_from_inside_to_outside_after_end).should be_false }
+    Then{ region.contain?(region_from_outside_before_start_to_inside).should be_false }
+
+    Then{ region.contain?(region_containing).should be_false }
+
+
+    Then{ region.contain?(region_outside_left).should be_false }
+    Then{ region.contain?(region_outside_right).should  be_false }
+    Then{ region.contain?(region_outside_extend_from_end).should be_false }
+    Then{ region.contain?(region_outside_extend_to_start).should be_false }
+
+
+  end
+
+  describe '#annotation' do
+    Then{ region.annotation.should == 'chr1:100..110,+' }
+    Then{ region_on_another_chromosome.annotation.should == 'chr2:103..107,+' }
+    Then{ region_on_another_strand.annotation.should == 'chr1:103..107,-' }
+  end
+
+  describe '#length' do
+    Then {Region.new('chr1', '+', 103, 104).length == 1 }
+    Then {Region.new('chr1', '+', 103, 106).length == 3 }
+    Then {Region.new('chr1', '-', 103, 106).length == 3 }
+  end
+
+  describe '.new_by_annotation' do
+    Then {
+      all_region_types.each{|region|
+        region.should == Region.new_by_annotation(region.annotation)
+      }
+    }
+  end
+
+  describe '==' do
+    Given(:same_region) { Region.new('chr1', '+', 100, 110) }
+    Given(:another_pos_start) { Region.new('chr1', '+', 103, 110) }
+    Given(:another_pos_end) { Region.new('chr1', '+', 100, 107) }
+    Given(:another_chromosome) { Region.new('chrY', '+', 100, 110) }
+    Given(:another_strand) { Region.new('chr1', '-', 100, 110) }
+
+    Then{ region.should == same_region}
+    Then{ region.should_not == another_pos_start}
+    Then{ region.should_not == another_pos_end}
+    Then{ region.should_not == another_chromosome}
+    Then{ region.should_not == another_strand}
+  end
+
+  describe 'hash/eql? ability' do
+    Given(:hash_by_region) { {region => 'first region', region_inside => 'another region'} }
+    Given(:same_region) { Region.new('chr1', '+', 100, 110) }
+    Then{ hash_by_region.should have_key(region) }
+    Then{ hash_by_region.should have_key(region) }
+    Then{ hash_by_region.should have_key(same_region) }
+    Then{ hash_by_region[region].should be_eql hash_by_region[same_region] }
+
+    Then{ hash_by_region.should have_key(region_inside) }
+
+    Then{ hash_by_region.should_not have_key(region_containing) }
   end
 end
