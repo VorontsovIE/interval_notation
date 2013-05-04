e3fd13bb31d74689a756232e6bab26b71ab6318d
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-05 02:50:09 +0400

    polyN-ning method moved to another file. fasta now extracting only transcripts having expression higher than threshold

--- extract_fasta.rb
+++ extract_fasta.rb
@@ -9,20 +9,7 @@ $:.unshift File.join(File.dirname(File.expand_path(__FILE__)), 'lib')
 require 'gene_data_loader'
 
 
-# marks best start (top `rate` part of them) with poly-N so that motif finding ignore them
-def mark_best_starts_as_poly_n(sequence, cages, rate)
-  new_sequence = sequence.dup
-  sum_cages = cages.inject(&:+)
-  cages_taken = 0
-  cages_sorted = cages.each_with_index.sort_by{|cage, index| cage}.reverse
-  while cages_taken + cages_sorted.first.first <= sum_cages * 0.7
-    cage, index = cages_sorted.shift
-    cages_taken += cage
-    new_sequence[index] = 'N'
-  end
-  new_sequence
-end
-
+min_expression = 1.0
 
 cages_file = 'prostate%20cancer%20cell%20line%253aPC-3.CNhs11243.10439-106E7.hg19.ctss.bed'
 framework = GeneDataLoader.new(cages_file, 'HGNC_protein_coding_22032013_entrez.txt', 'knownToLocusLink.txt', 'robust_set.freeze1.reduced.pc-3', 'knownGene.txt', 100)
@@ -31,6 +18,7 @@ genes_to_extract = framework.genes_to_process.select{|hgnc_id, gene| mtor_target
 
 File.open('weighted_5-utr.txt', 'w') do |fw|
   framework.output_all_5utr(genes_to_extract, fw) do |output_stream, gene_info, transcript_group, peaks_info, expression, spliced_sequence, spliced_cages|
+    next  unless expression >= min_expression
     output_stream.puts ">#{gene_info}\t#{transcript_group}\t#{expression}"
     output_stream.puts spliced_sequence
   end
@@ -38,6 +26,7 @@ end
 
 File.open('weighted_5-utr-polyN-masked.txt', 'w') do |fw|
   framework.output_all_5utr(genes_to_extract, fw) do |output_stream, gene_info, transcript_group, peaks_info, expression, spliced_sequence, spliced_cages|
+    next  unless expression >= min_expression
     output_stream.puts ">#{gene_info}\t#{transcript_group}\t#{expression}"
     output_stream.puts mark_best_starts_as_poly_n(spliced_sequence, spliced_cages, 0.7)
   end
--- lib/splicing.rb
+++ lib/splicing.rb
@@ -10,3 +10,18 @@ end
 def complement(sequence)
   sequence.tr('acgtACGT', 'tgcaTGCA')
 end
+
+
+# marks best start (top `rate` part of them) with poly-N so that motif finding ignore them
+def mark_best_starts_as_poly_n(sequence, cages, rate)
+  new_sequence = sequence.dup
+  sum_cages = cages.inject(&:+)
+  cages_taken = 0
+  cages_sorted = cages.each_with_index.sort_by{|cage, index| cage}.reverse
+  while cages_taken + cages_sorted.first.first <= sum_cages * 0.7
+    cage, index = cages_sorted.shift
+    cages_taken += cage
+    new_sequence[index] = 'N'
+  end
+  new_sequence
+end
