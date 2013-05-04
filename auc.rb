257f655b1aa5500c352903c9fbb4c229aae7494f
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-04 20:16:12 +0400

    script for calculating auc from curve points in file

---
+++ auc.rb
@@ -0,0 +1,13 @@
+$:.unshift File.join(File.dirname(File.expand_path(__FILE__)), 'lib')
+require 'classifier_quality'
+
+if $stdin.tty?
+  filename = ARGV.shift
+  raise 'Specify input file' unless filename
+  data = File.readlines(filename)
+else
+  data = $stdin.readlines
+end
+roc_points = data.map{|l| l.strip.split.map(&:to_f)}
+
+puts area_under_curve(roc_points)
