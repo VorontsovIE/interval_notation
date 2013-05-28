8eb7b87877e386fd0ddd37b4dfa8d46e9f43d689
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-29 00:46:30 +0400

    code for masking with poly-N refined

--- lib/splicing.rb
+++ lib/splicing.rb
@@ -2,7 +2,7 @@ $:.unshift File.dirname(File.expand_path(__FILE__))
 require 'genome_region'
 
 def splice_sequence(sequence, utr, exons_on_utr)
-  spliced_sequence = utr.splice(sequence, exons_on_utr).join  
+  spliced_sequence = utr.splice(sequence, exons_on_utr).join
   utr.strand == '+'  ?  spliced_sequence  :  complement(spliced_sequence)
 end
 
@@ -11,17 +11,34 @@ def complement(sequence)
   sequence.tr('acgtACGT', 'tgcaTGCA')
 end
 
+def mark_single_best_start_as_poly_n(sequence, cages, window_size)
+  new_sequence = sequence.dup
+  cages_taken = 0
+  cage, index = cages.each_with_index.sort_by{|cage, index| cage}.last
+
+  (index-window_size..index+window_size).each do |pos|
+    next if pos < 0
+    cages_taken += (cages[pos] || 0)
+    new_sequence[pos] = 'N'
+  end
+
+  new_sequence
+end
 
 # marks best start (top `rate` part of them) with poly-N so that motif finding ignore them
-def mark_best_starts_as_poly_n(sequence, cages, rate)
+def mark_best_starts_as_poly_n(sequence, cages, rate, window_size)
   new_sequence = sequence.dup
   sum_cages = cages.inject(&:+)
   cages_taken = 0
   cages_sorted = cages.each_with_index.sort_by{|cage, index| cage}.reverse
-  while cages_taken + cages_sorted.first.first <= sum_cages * 0.7
+  while cages_taken < sum_cages * rate
     cage, index = cages_sorted.shift
-    cages_taken += cage
-    new_sequence[index] = 'N'
+    (index-window_size..index+window_size).each do |pos|
+      next if pos < 0 || pos >= cages.size
+      next if new_sequence[pos] == 'N'
+      cages_taken += cages[pos]
+      new_sequence[pos] = 'N'
+    end
   end
   new_sequence
 end
