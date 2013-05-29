c24e37b3f56f2d25ee6b3b1faeb22b810300ed08
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-29 23:09:07 +0400

    convert fasta to plain (extension changed)

--- convert_fasta_to_plain.rb
+++ convert_fasta_to_plain.rb
@@ -1,5 +1,7 @@
 Dir.glob('for_article/source_data/hg18/*.fa') do |filename|
-  File.open(filename + 'sta', 'w') do |fw|
+  basename = File.basename(filename, File.extname(filename))
+  dirname = File.dirname(filename)
+  File.open(File.join(dirname, "#{basename}.plain"), 'w') do |fw|
     File.open(filename) do |f|
       f.each_line do |line|
         next  if line.start_with?('>')
