6f507d00c15a5cf39a9808be37716d1d43153e81
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-05-30 23:43:20 +0400

    mix of cages refined

--- mix_cages.rb
+++ mix_cages.rb
@@ -6,9 +6,30 @@ cage_files.each do |filename|
   raise ArgumentError, "File #{filename} doesn't exist"  unless File.exist?(filename)
 end
 
-cages = {}
-cage_files.each do |filename|
-  add_cages(cages, read_cages(filename))
+#cages = {}
+#cage_files.each do |filename|
+#  add_cages(cages, read_cages(filename))
+#end
+
+all_cages = cage_files.map{|filename| read_cages(filename)}
+
+cages = sum_cages( all_cages )
+
+cages.keys.each do |strand|
+  cages[strand].keys.each do |chromosome|
+    cages[strand][chromosome].keys.each do |pos|
+      cages[strand][chromosome].delete(pos)  if all_cages.count{|cg| cg[strand][chromosome][pos] } <= 1
+    end
+  end
 end
+
 mul_cages_inplace(cages, 1.0 / cage_files.size)
+
+cages.keys.each do |strand|
+  cages[strand].keys.each do |chromosome|
+    cages[strand][chromosome].each do |pos, val|
+      cages[strand][chromosome][pos] = val.floor
+    end
+  end
+end
 print_cages(cages, $stdout)
