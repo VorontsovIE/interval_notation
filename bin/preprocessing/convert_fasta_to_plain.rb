18e0d3444bcbacd856ad966d2e562f8d0615e1c3
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-02-05 01:56:30 +0400

    make fasta-->plain coverter more convenient to use

--- bin/preprocessing/convert_fasta_to_plain.rb
+++ bin/preprocessing/convert_fasta_to_plain.rb
@@ -1,7 +1,11 @@
-Dir.glob('for_article/source_data/hg18/*.fa') do |filename|
+dirname = ARGV.shift
+
+Dir.glob(File.join(dirname, '*.fa')).sort.each do |filename|
   basename = File.basename(filename, File.extname(filename))
   dirname = File.dirname(filename)
-  File.open(File.join(dirname, "#{basename}.plain"), 'w') do |fw|
+  filename_plain = File.join(dirname, "#{basename}.plain")
+  $stderr.print "#{filename} --> #{filename_plain} started"
+  File.open(filename_plain, 'w') do |fw|
     File.open(filename) do |f|
       f.each_line do |line|
         next  if line.start_with?('>')
@@ -9,4 +13,5 @@ Dir.glob('for_article/source_data/hg18/*.fa') do |filename|
       end
     end
   end
+  $stderr.print " --> complete\n"
 end
