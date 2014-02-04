9f5d50789ef413c86636353f9c378accbf84d2c0
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-02-05 02:07:41 +0400

    remove notification about Sequence class from src (it's already in ToDo)

--- obtain_genes.rb
+++ obtain_genes.rb
@@ -5,10 +5,6 @@ $logger = Logger.new($stderr)
 
 require_relative 'lib/gene_data_loader'
 
-# class Sequence
-#   attr_reader :sequence, :markup
-# end
-
 # cages_file = 'prostate%20cancer%20cell%20line%253aPC-3.CNhs11243.10439-106E7.hg19.ctss.bed'
 # output_file = 'spliced_transcripts.txt'
 
@@ -16,7 +12,6 @@ cages_file, output_file = *ARGV
 raise "You should specify file with cages for a specific tissue(*.bed) and output file" unless cages_file && output_file
 
 framework = GeneDataLoader.new(cages_file, 'HGNC_protein_coding_22032013_entrez.txt', 'knownToLocusLink.txt', 'robust_set.freeze1.reduced.pc-3', 'knownGene.txt', 100, 'source_data/genome/hg19')
-
 
 File.open(output_file, 'w') do |fw|
   framework.output_all_5utr(framework.genes_to_process, fw)
