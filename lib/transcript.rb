15663dde31f50e7ed0bc5465d9b3147f69b646e9
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-02-06 20:20:59 +0400

    transcript expansion refactored a lot, added help about peak trimming/expansion

--- lib/transcript.rb
+++ lib/transcript.rb
@@ -14,7 +14,7 @@ class Transcript
   # We don't remove version from name not to have a bug with  uc010nxr.1(chr1)  and  uc010nxr.2(chrY)
   # Be careful about real versions of transcripts, use synchronized databases
   def self.new_by_infos(info)
-    name, chromosome, strand, tx_start, tx_end, cds_start, cds_end, exon_count, exon_starts, exon_ends, protein_id, _align_id = info.split("\t")
+    name, chromosome, strand, _tx_start, _tx_end, cds_start, cds_end, exon_count, exon_starts, exon_ends, protein_id, _align_id = info.split("\t")
     exon_starts = exon_starts.split(',').map(&:strip).reject(&:empty?).map(&:to_i)
     exon_ends = exon_ends.split(',').map(&:strip).reject(&:empty?).map(&:to_i)
     coding_region = GenomeRegion.new(chromosome, strand, cds_start.to_i, cds_end.to_i)  ### rescue nil
@@ -40,14 +40,17 @@ class Transcript
     raise "5'-UTR of non-coding gene is undefined"  unless coding?
     full_gene_region - coding_region.with_downstream(Float::INFINITY)
   end
+
   def expanded_upstream(region_length)
     Transcript.new(name, chromosome, strand, coding_region, exons.with_upstream(region_length), protein_id)
   end
+  private :expanded_upstream
+
   def utr_5_with_upstream(region_length)
     # we should first expand, then take utr because some transcripts in UCSC don't have utr, their start marked at the same place, their coding region starts
     expanded_upstream(region_length).utr_5
   end
-  private :utr_5_with_upstream, :expanded_upstream
+  private :utr_5_with_upstream
 
   # region_length is length of region before txStart(start of transcript) where we are looking for peaks
   def associate_peaks(peaks, region_length)
@@ -60,30 +63,60 @@ class Transcript
 
   def calculate_exons_on_utr(peaks)
     @exons_on_utr = begin
-      exons_on_utr_unexpanded = exons.with_upstream(region_length) & utr_5_with_upstream(region_length)
-      # expand ROI to include those peaks that intersect ROI
-      region_expanded_upstream_with_peaks(exons_on_utr_unexpanded, peaks)
+      expanded_transcript = expanded_upstream(region_length).expand_and_trim_with_peaks(peaks)
+      expanded_transcript.exons & expanded_transcript.utr_5
     end
   end
   private :calculate_exons_on_utr
 
   # utr_region is defined by leftmost peak intersecting region [txStart-region_length; coding_region_start) and by start of coding region
   def utr_region
-    region_expanded_upstream_with_peaks(utr_5_with_upstream(region_length), peaks_associated)
+    expanded_upstream(region_length).expand_and_trim_with_peaks(peaks_associated).utr_5
   end
 
-  # expand region to most upstream peaks and trim at that point (so result can be even shorter if peaks located downstream from start)
-  def region_expanded_upstream_with_peaks(region, peaks)
-    peaks_intersecting_region = peaks.select{|peak| region.intersect?(peak.region) }
+  #
+  # Elongate region to the most upstream peak which intersects transcript and trims at this point start
+  # It gives us transcript lasting upstream to the last point with reasonable expression (i.e. to a peak)
+  # Usually this procedure is applied after transcript was elongated 100-1000 bp from annotated txStart
+  # so +#expand_and_trim_with_peaks+ usually trims over-expanded transcript to its real start
+  # But if transcript intersects peak in its(peak) middle, we elongate transcript to take whole the peak
+  #
+  # Note that first-stage elongation should be done explicit with +#expanded_upstream+!
+  #
+  #
+  # First case - trimming when all peaks intersecting with transcript are downstream to transcript start:
+  # transcript                                 (_____)^^^^(___)^(_______)
+  # peaks                                         (.)  (..)
+  # most_upstream_peak                            (.)
+  # peak_with_downstream                          (............................
+  # upstream of transcript  ...................)
+  # region expansion                          Empty
+  # exons_expanded (result)                       (__)^^^^(___)^(_______)
+  #
+  #
+  # Second case - intersecting peak expands transcript region when it intersects with transcript but lies upstream to its start:
+  # transcript                                 (_____)^^^^(___)^(_______)
+  # peaks                             (..) (.....) (.)
+  # most_upstream_peak                     (.....)
+  # peak_with_downstream                   (.................................
+  # upstream of transcript  ...................)
+  # region expansion                       (...)
+  # exons_expanded (result)                (_________)^^^^(___)^(_______)
+  #
+  def expand_and_trim_with_peaks(peaks)
+    transcript_region = full_gene_region
+    peaks_intersecting_region = peaks.select{|peak| peak.intersect?(transcript_region) }
     if peaks_intersecting_region.empty?
-      region
+      self
     else
       most_upstream_peak = peaks_intersecting_region.map(&:region).min
       peak_with_downstream = most_upstream_peak.with_downstream(Float::INFINITY)
-      region_expansion = region.most_upstream_region.upstream(Float::INFINITY).intersection(peak_with_downstream)
-      region.union(region_expansion).intersection(peak_with_downstream)
+      region_expansion = exons.upstream(Float::INFINITY) & peak_with_downstream
+      exons_expanded = (exons | region_expansion) & peak_with_downstream
+      Transcript.new(name, chromosome, strand, coding_region, exons_expanded, protein_id)
     end
   end
+
 
   # ucsc_id => transcript
   def self.transcripts_from_file(input_file)
