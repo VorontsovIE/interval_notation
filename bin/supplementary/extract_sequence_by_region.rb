2dcf23b8b0f67e28f4fa5ddadaf662c53b389a05
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-11-16 06:18:09 +0300

    make sequence extractor customizable

--- bin/supplementary/extract_sequence_by_region.rb
+++ bin/supplementary/extract_sequence_by_region.rb
@@ -1,12 +1,26 @@
 require_relative '../../lib/intervals/genome_region'
+require 'optparse'
 
-begin
-annotation = ARGV[0]
-genome_folder = ARGV[1]
+tab_separated = false
+print_positions = true
+OptionParser.new do |opts|
+  opts.banner = "Tool allows one to load sequence of the region in a genome. " +
+                "Genome should be in a plain text without line breaks or FASTA > annotation string\n" +
+                "No result reversing to be done for regions on negative strand\n" +
+                "Usage: #{opts.program_name} <genome_folder> <region of interest> [options]\n"
+  opts.separator "Options:"
+  opts.on('--[no-]tab-separated', 'Output results with tabs between nucleotides (useful for pasting to tables') {|v| tab_separated = v }
+  opts.on('--[no-]print-positions', 'Output position numbers above cage counts; works only with --tab-separated') {|v| print_positions = v }
+end.parse!(ARGV)
+
+genome_folder = ARGV[0]
+annotation = ARGV[1]
 region_of_interest = GenomeRegion.new_by_annotation(annotation)
 
-puts region_of_interest.load_sequence(genome_folder)
-rescue => e
-  $stderr.puts "Annotation should be in format: chr1:1234..5678,+\n\n"
-  $stderr.puts e
+sequence = region_of_interest.load_sequence(genome_folder)
+if tab_separated
+  puts region_of_interest.region.to_range.to_a.join("\t")  if print_positions
+  puts sequence.each_char.to_a.join("\t")
+else
+  puts sequence
 end
